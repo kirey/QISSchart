a6b1dc3e996a15cdb59c75067366559188940c81
Natalija <kapljicaa@gmail.com>
2018-10-24 10:27:28 +0200

    Model 1, 2, 3 demo ppts (separately). Code refactoring.

--- BubbleChart/src/Main.java
+++ BubbleChart/src/Main.java
@@ -72,56 +72,7 @@ public class Main {
 		File pptFileModel4 = new File("Model4.pptx");
 		//Model4.generateModel(pptFileModel4);
 		
-		/*//CREATE SLIDE SHOW
-		XMLSlideShow ppt = new XMLSlideShow();	
-		XSLFSlideMaster slideMaster = ppt.getSlideMasters()[0];
-		XSLFSlideLayout slideLayout = slideMaster.getLayout(SlideLayout.BLANK);
-		
-		XSLFSlide slide = ppt.createSlide(slideLayout);
-		//XSLFSlide slide1 = ppt.createSlide(slideMaster.getLayout(SlideLayout.TITLE_AND_CONTENT));
-		XSLFSlide slide2 = ppt.createSlide();
-		
-		
-		//CREATING CHART PNGS
-		File bubbleChart = new File("BubbleChart.png");
-		File pieChartConformity = new File("ConformityPieChart.png");		
-		File pieChartRobustness = new File("RobustnessPieChart.png");	
-		File barChartRobustness = new File("RobustnessBarChart.png");
-		File barStackedChartRobustness = new File("RobustnessBarStackedChart.png");
-		File barStackedChartConformity = new File("ConformityBarStackedChart.png");
-		File ringPlot = new File("RingPlot.png");
-
-		Dimension pgsize = ppt.getPageSize();
-	    int pgx = pgsize.width; //slide width in points
-	    int pgy = pgsize.height; //slide height in points
-		
-		int width = 560;  Width of the image 
-		int height = 370;  Height of the image 
-		ChartUtilities.saveChartAsPNG(bubbleChart, Charts.createBubbleChart(createDataset()), width, height);
-		ChartUtilities.saveChartAsPNG(pieChartRobustness, Charts.createPieChart3D(createDatasetPie(4.8, 3.6), "Robustness"), 330, 330);
-		ChartUtilities.saveChartAsPNG(pieChartConformity, Charts.createPieChart(createDatasetPie(100.0, 99.94), "Conformity"), 330, 330);
-		ChartUtilities.saveChartAsPNG(barChartRobustness, Charts.createBarChart(createProgressBarDataset(), "Robustness"), 200, 350);
-		ChartUtilities.saveChartAsPNG(barStackedChartRobustness, Charts.createStackedBarChart(createStackedBarDataset(3.6, 4.8, 4.0), "Robustness"), 350, 150);
-		ChartUtilities.saveChartAsPNG(barStackedChartConformity, Charts.createStackedBarChart(createStackedBarDataset(99.94, 100, 95.0), "Conformity"), 350, 150);
-		ChartUtilities.saveChartAsPNG(ringPlot, Charts.createRingChart(createDatasetPie(4.8, 3.6), "Robustness"), 200, 200);
-		//createStackedBarDataset
-		System.out.println("Image rendered!");
-
-		
-		//ADDING ELEMENTS ON SLIDE
-		File file = new File("PowerPointBubble.pptx");
-		File pptFileModel1 = new File("Model1.pptx");
-        createSlideTitle(slide, "Model 1");
-		createPresentationWithImage(bubbleChart, file, ppt, slide, new Rectangle(pgx/2, 35, (int)(560*0.60), (int)(370*0.60)));
-		createPresentationWithImage(pieChartRobustness, file, ppt, slide, new Rectangle(35, pgy/2-35, (int)(330*0.6), (int)(330*0.6)));
-		createPresentationWithImage(pieChartConformity, file, ppt, slide, new Rectangle(pgx/2, pgy/2-35, (int)(330*0.6), (int)(330*0.6)));
-		//createPresentationWithImage(barChartRobustness, file, ppt, slide, new Rectangle(35, pgy/2, (int)(200*0.60), (int)(350*0.60)));
-		createPresentationWithImage(barStackedChartRobustness, file, ppt, slide, new Rectangle(35, pgy/2+165, (int)(350*0.60), (int)(150*0.60)));
-		createPresentationWithImage(barStackedChartConformity, file, ppt, slide, new Rectangle(pgx/2, pgy/2+165, (int)(350*0.60), (int)(150*0.60)));
-		createTableOnSlide(file, ppt, slide);
-		createPresentationWithImage(ringPlot, file, ppt, slide2, new Rectangle(35, pgy/2, (int)(200*0.60), (int)(200*0.60)));
-		Model2.createTableModel2(file, ppt, slide2);
-
+	/*
 		//////TEST///// inserting image to layout content
 		
 		//String img = "BubbleChart.png";
@@ -146,68 +97,10 @@ public class Main {
 		//shp.drawContent((Graphics2D) myPicture.getGraphics());
 		
 		XSLFTextShape title2 =  slide1.getPlaceholder(0);
-		title2.setText("Model 2");
-		
-		
-		//-----slide 2-----
-		//**XSLFGroupShape group1 = slide2.createGroup(); 
-		
-		//**picture = group1.createPicture(idx); 
-				
-		///////////////
-		FileOutputStream out = new FileOutputStream(file);
-		ppt.write(out);
-		System.out.println("PowerPoint with image created!");
-
-		out.close();
-
-		showAvailableSlideLayouts();*/
-	}
-
-	private static void createSlideTitle(XSLFSlide slide, String string) {
-		//ADD TEXT FOR TITLE
-		XSLFTextBox shape = slide.createTextBox();
-		shape.setAnchor(new Rectangle(250, 5, 150, 25));
-		XSLFTextParagraph p = shape.addNewTextParagraph();
-		p.setTextAlign(TextAlign.CENTER);
-
-		XSLFTextRun r1 = p.addNewTextRun();
-		r1.setText("Model 1");
-		r1.setFontColor(Color.BLACK);
-		r1.setFontSize(24.);
-		
-	}
-
-	/*//CHART
-	private static XYZDataset createDataset() {
-		DefaultXYZDataset dataset = new DefaultXYZDataset();
-
-		dataset.addSeries("GI",new double[][] { { 3,5 }, // X-Value
-						{ 50,60 }, // Y-Value
-						{ 20,30 } // Z-Value
-				});
-		dataset.addSeries("GT", new double[][] { { 1 }, { 75 }, { 20 } });
-		
-		return dataset;
-	}
+		title2.setText("Model 2");		 */
 	
-	
-	
-	//PIE
-	private static PieDataset createDatasetPie( Double target, Double currentVal) {
-	      DefaultPieDataset dataset = new DefaultPieDataset( );
-	      dataset.setValue( "Robustness", currentVal);  
-	      dataset.setValue( "Target", Math.abs(target-currentVal));   
-	      return dataset;         
 	}
-		
-	private static CategoryDataset createProgressBarDataset() {
-		
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();		 
-		dataset.addValue(4.0, "Target", "");
-		dataset.addValue(3.6, "Index", "");
-		return dataset;		
-	}*/
+
 	
 	private static JFreeChart createBarChart(CategoryDataset dataset, String chartName ) {
 		 JFreeChart chart=ChartFactory.createBarChart(
@@ -240,26 +133,6 @@ public class Main {
          return chart;
 	}
 	
-	/*private static CategoryDataset createStackedBarDataset(double index, double target, double threshold) {
-		  double[][] data = new double[][]{
-		  {210},
-		  {200},
-		  };
-		  CategoryDataset ds =  DatasetUtilities.createCategoryDataset(
-		  "Team ", "Match", data);
-		
-		double index = 3.6;
-		double target = 4.8;
-		double threshold = 4.0;
-		
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();	
-		
-		dataset.addValue(index, "Index", "");
-		dataset.addValue(Math.abs(threshold-index), "Treshold", "");
-		dataset.addValue(target - Math.max(index,threshold), "Target", "");
-		
-		return dataset;	
-	}*/
 	
 	public static List<DemoDataObj> demoData(){
 		
@@ -285,96 +158,6 @@ public class Main {
 		
 		return demoData;
 		
-	}
-
-	private static void createTableOnSlide(File file, XMLSlideShow ppt, XSLFSlide slide) throws IOException {
-
-		// Add table to ppt on the same page
-		
-		int numColumns = 3;
-		int numRows = demoData().size();
-				
-		XSLFTable tbl = slide.createTable();
-		
-		Dimension pgsize = ppt.getPageSize();
-	    int pgx = pgsize.width; //slide width in points
-	    int pgy = pgsize.height; //slide height in points
-		
-		tbl.setAnchor(new Rectangle(25, 50, pgx/2 - 100, 25*(numRows+1)));
-		
-		XSLFTableRow headerRow = tbl.addRow();
-		headerRow.setHeight(15);
-		String[] headers = {"Area","Robustness","Conformity"};
-		
-		// header
-		for (int i = 0; i < numColumns; i++) {
-			XSLFTableCell th = headerRow.addCell();
-			XSLFTextParagraph p = th.addNewTextParagraph();
-			p.setTextAlign(TextAlign.CENTER);
-			XSLFTextRun r = p.addNewTextRun();
-			r.setText(headers[i]);
-			r.setBold(true);
-			r.setFontSize(10);
-			th.setFillColor(new Color(79, 129, 189));
-			// th.setBorderWidth(BorderEdge.bottom, 2.0);
-			// th.setBorderColor(BorderEdge.bottom, Color.white);
-
-			tbl.setColumnWidth(i, 150); // all columns are equally sized
-		}
-
-		//rows        
-		for (int rownum = 0; rownum < numRows; rownum++) {
-			XSLFTableRow tr = tbl.addRow();
-			tr.setHeight(15);
-		
-			DemoDataObj dataRow = demoData().get(rownum);
-			
-			XSLFTableCell cell = tr.addCell();
-			XSLFTextParagraph p = cell.addNewTextParagraph();
-			XSLFTextRun r = p.addNewTextRun();
-			r.setText(dataRow.getCompany());
-			r.setFontSize(10);
-			p.addLineBreak();
-			
-			r = p.addNewTextRun();			
-			r.setText("Robustness");
-			r.setFontSize(6);
-						
-			
-			cell = tr.addCell();
-			p = cell.addNewTextParagraph();
-			r = p.addNewTextRun();
-			r.setFontSize(10);
-			r.setText(dataRow.getRobustness());
-			
-			cell = tr.addCell();
-			p = cell.addNewTextParagraph();
-			r = p.addNewTextRun();
-			r.setFontSize(10);
-			r.setText(dataRow.getConformity());
-			
-		}
-		
-	}
-
-	private static void createPresentationWithImage(File image, File file, XMLSlideShow ppt, XSLFSlide slide, Rectangle rectangle)
-			throws IOException {
-		// XMLSlideShow ppt = new XMLSlideShow();
-		// XSLFSlide slide = ppt.createSlide();
-		
-		byte[] picture = IOUtils.toByteArray(new FileInputStream(image));
-		int idx = ppt.addPicture(picture, XSLFPictureData.PICTURE_TYPE_PNG);
-		XSLFPictureShape pic = slide.createPicture(idx);
-		pic.setAnchor(rectangle);
-		
-
-		// File file = new File("PowerPointBubble.pptx");
-		/*
-		 * FileOutputStream out = new FileOutputStream(file); ppt.write(out);
-		 * System.out.println("PowerPoint with image created!");
-		 * 
-		 * out.close();
-		 */
 	}
 
 	public static void showAvailableSlideLayouts() {
